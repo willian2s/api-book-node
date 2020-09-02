70bcc627419d9bbb8d9a5f3c58a89555125e8473
Willian Silva <silva.williansantos@gmail.com>
2020-09-02 00:28:04 +0000

    Setup Test Integration and first case

--- src/app.js
+++ src/app.js
@@ -6,4 +6,10 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World'));
 
+app.get('/products', (req, res) => res.send([{
+  name: 'Default product',
+  description: 'product description',
+  price: 100,
+}]))
+
 export default app;
--- test/integration/helpers.js
+++ test/integration/helpers.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest';
 import chai from 'chai';
-import app from '../../src/app';
+import app from '../../src/app.js';
 
 global.app = app;
 global.request = supertest(app);
